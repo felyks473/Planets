059ab60eaa97de7e7ba2ca033b21f21b5c5c81c6
felyks473 <75755589+felyks473@users.noreply.github.com>
2025-07-12 10:19:45 +0200

    Implemented simple physics system

--- Engine/src/Components/include/TransformComponent.h
+++ Engine/src/Components/include/TransformComponent.h
@@ -12,12 +12,25 @@ namespace Planets {
     class TransformComponent 
     {
     public:
-        TransformComponent() = default;
+        TransformComponent();
         ~TransformComponent() = default;
     
         void Update(int windowWidth, int windowHeight, std::shared_ptr<Shader> shader, int shader_flag);
     private:
         glm::mat4 view;
+        glm::vec3 earthPosition;
+        glm::vec3 moonPosition;
+        float earthOrbitSpeed;
+        float angleDegree;
+        float angleRadians;
+        float distanceFromEarthX;
+        float distanceFromEarthZ;
+        float distanceFromMoonX;
+        float distanceFromMoonZ;
+        float days;
+        glm::vec3 earthAxis;
+        glm::vec3 sunAxis;
+        glm::vec3 moonAxis;
     };
 
 }
--- Engine/src/Components/src/TransformComponent.cpp
+++ Engine/src/Components/src/TransformComponent.cpp
@@ -5,56 +5,97 @@
 
 namespace Planets {
 
+    TransformComponent::TransformComponent()
+    : earthPosition(glm::vec3(0.0f, 0.0f, 0.0f)), moonPosition(glm::vec3(0.0f, 0.0f, 0.0f)), angleDegree(23.5f)
+    {
+        earthOrbitSpeed = 360.0f / (365.25f * 24.0f * 60.0f * 60.0f); // 365.25 days is  earth's orbital period 
+        
+        /* 1e7f is a scale factor */
+        distanceFromEarthX = 149.6e6f / 1e7f;
+        distanceFromEarthZ = 149.2e6f / 1e7f;
+        distanceFromMoonX = 0.384f * 10.f;
+        distanceFromMoonZ = 0.384f * 10.f;
+        angleRadians = glm::radians(angleDegree);
+        
+
+        /*               Earth's rotation period - 23h 56m 4s         */
+        /*               We get this number by doing this:            */
+        /* 23 / 24 [h] + 56 / (24 * 60) [m] + 4 / (24 * 60 * 60) [s]  */
+        
+        days = (0.95833f + 0.03889f + 0.00004629f);
+
+        earthAxis = glm::normalize(glm::vec3(sin(angleRadians), cos(angleRadians), 0.0f));
+        sunAxis = glm::normalize(glm::vec3(sin(glm::radians(7.25f)), cos(glm::radians(7.25f)), 0.0f));
+        moonAxis = glm::normalize(glm::vec3(sin(glm::radians(5.14f)), cos(glm::radians(5.14f)), 0.0f));
+    }
+
     void TransformComponent::Update(int windowWidth, int windowHeight, std::shared_ptr<Shader> shader, int shader_flag)
     {
         shader->Use();
 
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 tilt = glm::mat4(1.0f);
+
+        /* I believe that for the true tidal lock of a moon the scale factor below has to be the same as the scale factor of the distance from earth */
+        /* That's either going to make the moon's distance too large or make the spinning of the moon too slow                                       */
+        /* At one point I'll try to get them in sync but for now let them be slightly inaccurate                                                     */
+
+        float earthSpinningSpeed = 360.0f / (days * 24.0f * 60.0f * 60.0f) * static_cast<float>(glfwGetTime()) * 1e4f;
+        float moonSpinningSpeed = 360.0f / (27.321661f * 24.0f * 60.0f * 60.0f) * static_cast<float>(glfwGetTime()) * 1e4f; //27.321661 days
     
+        float sunSpinningSpeed = 360.0f / (25.0f * 24.0f * 60.0f * 60.0f) * static_cast<float>(glfwGetTime()) * 1e4f; // approx. 25 days
+
         if (shader_flag == 0) // Earth
         {
+            model = glm::translate(model, earthPosition);
             tilt = glm::rotate(tilt, glm::radians(23.5f), glm::vec3(1.0f, 0.0f, 0.0f));
+            /* 1e5f is a scale factor */
+            earthPosition.x = distanceFromEarthX * cos(earthOrbitSpeed * glfwGetTime() * 1e5f);
+            earthPosition.z = distanceFromEarthZ * sin(earthOrbitSpeed * glfwGetTime() * 1e5f);
             
             model = model * tilt;
 
-            view = glm::translate(glm::mat4(1.0f), glm::vec3(-10.0f, -5.0f, -30.0f));
-            float angle = 25.0f * static_cast<float>(glfwGetTime());
-            float angleDegree = 23.5f;
-    
-            float angleRadians = glm::radians(angleDegree);
-
-            glm::vec3 axis = glm::normalize(glm::vec3(sin(angleRadians), cos(angleRadians), 0.0f));
-            model = glm::rotate(model, glm::radians(angle), axis);
+            model = glm::rotate(model, glm::radians(earthSpinningSpeed), earthAxis);
         }
         if (shader_flag == 1) // Sun
         {
-            model = glm::translate(model, glm::vec3(10.0f, 10.0f, 0.0f));
-            model = glm::scale(model, glm::vec3(2.0f, 2.0f, 1.0f));
+            model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
             tilt = glm::rotate(tilt, glm::radians(7.25f), glm::vec3(1.0f, 0.0f, 0.0f));
         
+            model = glm::rotate(model, glm::radians(sunSpinningSpeed), sunAxis);
+            
             model = model * tilt;
-
-            view = glm::translate(glm::mat4(1.0f), glm::vec3(-10.0f, -5.0f, -30.0f));
         }
         if (shader_flag == 2) // Moon
         {
-            model = glm::translate(model, glm::vec3(5.0f, 5.0f, 0.0f));
-            model = glm::scale(model, glm::vec3(0.5f, 0.5f, 1.0f));
-            tilt = glm::rotate(tilt, glm::radians(5.14f), glm::vec3(1.0f, 0.0f, 0.0f));
-        
-            model = model * tilt;
+            glm::mat4 moon_model = glm::mat4(1.0f);
+            glm::mat4 earth_model = glm::mat4(1.0f);
+            
+            /* 1e5f is a scale factor */
+            earthPosition.x = distanceFromEarthX * cos(earthOrbitSpeed * glfwGetTime() * 1e5f);
+            earthPosition.z = distanceFromEarthZ * sin(earthOrbitSpeed * glfwGetTime() * 1e5f);
+            earth_model = glm::translate(earth_model, earthPosition);         
 
-            view = glm::translate(glm::mat4(1.0f), glm::vec3(-5.0f, -10.0f, -30.0f));
+            moonPosition.x = distanceFromMoonX * cos(moonSpinningSpeed);
+            moonPosition.z = distanceFromMoonZ * sin(moonSpinningSpeed);
+        
+            moon_model = glm::rotate(moon_model, glm::radians(5.14f), glm::vec3(1.0f, 0.0f, 0.0f));
+            moon_model = glm::rotate(moon_model, glm::radians(moonSpinningSpeed), moonAxis);
+            moon_model = glm::translate(moon_model, moonPosition);
+        
+            moon_model = glm::scale(moon_model, glm::vec3(0.25f, 0.25f, 0.25f));
+
+            model = earth_model * moon_model;
         }
         if (shader_flag == 3)
         {
             model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
             model = glm::scale(model, glm::vec3(50.0f, 50.0f, 50.0f));
-
-            view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
         }
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(windowWidth) / static_cast<float>(windowHeight), 0.1f, 100.0f);
+        
+        view = glm::lookAt(glm::vec3(0.0f, 0.0f, -70.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(windowWidth) / static_cast<float>(windowHeight), 0.1f, 1000.0f);
 
         glm::mat4 mvp = projection * view * model;
 
